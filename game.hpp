9e034107c2f73ce6c84aa79670d1e2b0b95af8a9
FMJ2000 <martin.b.hanekom@gmail.com>
2021-07-10 11:58:18 +0200

    initial codebase

---
+++ game.hpp
@@ -0,0 +1,24 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <stack>
+#include <SFML/Graphics.hpp>
+
+#define NAME "kapenaars"
+
+class GameState;
+
+class Game {
+	public:
+		std::stack<GameState *> states;
+		sf::RenderWindow window;
+		void pushState(GameState * state);
+		void popState();
+		void changeState(GameState * state);
+		GameState * peekState();
+		void gameLoop();
+		Game();
+		~Game();
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "game.hpp"
+
+int main() {
+	Game game;
+	game.gameLoop();
+	return 0;
+}
